11acda565154b2b0c1f5690fef91ef39fe0c6458
MishaAprelskaya <mikhal.aprelskaya@gmail.com>
2021-01-20 01:12:26 +0300

    module0 is done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+	if (n === 0 | n === 1) {
+		return 1;
+	} else {
+		let result = 1;
+		for (let i = 0; i < n; i++) {
+			result = result * (n - i);
+		}
+		return result;
+	}
 }
+
+console.log(factorial(5));
