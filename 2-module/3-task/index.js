1294f1bb192b11800a5138d1b2bc36a5797d9fb4
MishaAprelskaya <mikhal.aprelskaya@gmail.com>
2021-01-24 18:34:05 +0300

    module 2 task 1 and 2 are done

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
-};
+	x: undefined,
+	y: undefined,
+
+	read: function (a, b) {
+		x = a;
+		y = b;
+	},
+
+	sum: function () {
+		return x + y;
+	},
+
+	mul: function () {
+		return x * y;
+	}
+}
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
